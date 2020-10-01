635e3b9111713305fd201c23dfa9322f7f7514ef
Fúlvio B. Nedel <fulvionedel@gmail.com>
2020-10-01 03:48:11 -0300

    .

--- R/codestados.R
+++ R/codestados.R
@@ -10,8 +10,8 @@
 #'
 codestados <- function(){
   data.frame(coduf = c(11:17, 21:29, 31:33, 35, 41:43, 50:53),
-        siglauf = c("RO", "AC", "AM", "RR", "PA", "AP", "TO", "MA", "PI", "CE", "RN", "PB", "PE", "AL", "SE", "BA", "MG", "ES", "RJ", "SP", "PR", "SC", "RS", "MS", "MT", "GO", "DF"),
-        nomeuf = c("Rond\U00f4nia", "Acre", "Amazonas", "Roraima", "Par\U00e1", "Amap\U00e1", "Tocantins", "Maranh\U00e3o", "Piau\U00ed", "Cear\U00e1", "Rio Grande do Norte", "Para\U00edba", "Pernambuco", "Alagoas", "Sergipe", "Bahia", "Minas Gerais", "Esp\U00edrito Santo", "Rio de Janeiro", "S\U00e3o Paulo", "Paran\U00e1", "Santa Catarina", "Rio Grande do Sul", "Mato Grosso do Sul", "Mato Grosso", "Goi\U00e1s", "Distrito Federal")
+             siglauf = c("RO", "AC", "AM", "RR", "PA", "AP", "TO", "MA", "PI", "CE", "RN", "PB", "PE", "AL", "SE", "BA", "MG", "ES", "RJ", "SP", "PR", "SC", "RS", "MS", "MT", "GO", "DF"),
+             nomeuf = c("Rond\U00f4nia", "Acre", "Amazonas", "Roraima", "Par\U00e1", "Amap\U00e1", "Tocantins", "Maranh\U00e3o", "Piau\U00ed", "Cear\U00e1", "Rio Grande do Norte", "Para\U00edba", "Pernambuco", "Alagoas", "Sergipe", "Bahia", "Minas Gerais", "Esp\U00edrito Santo", "Rio de Janeiro", "S\U00e3o Paulo", "Paran\U00e1", "Santa Catarina", "Rio Grande do Sul", "Mato Grosso do Sul", "Mato Grosso", "Goi\U00e1s", "Distrito Federal")
         )
 }
 
--- R/plotZ.R
+++ R/plotZ.R
@@ -1,11 +1,27 @@
 #' Gráfico da probababilidade de pertencer a uma área da curva Normal
 #' @aliases plotZ
 #' 
+#' @param x valor a comparar com a média
+#' @param mu média
+#' @param dp desvio-padrão
+#' @param p probabilidade
+#' @param z escore-z
+#' @param cor cor do preenchimento da área sob a curva
+#' @param main título
+#' @param sub subtítulo
+#' @param sentido "menor" (padrão) calcula e desenha a probabilidade de um valor menor ou igual a x, p ou z; "maior" calcula e desenha a probabilidade de um valor maior que x, p ou z
+#' @param cex.main tamanho da fonte do título
+#' @param cex.sub  tamanho da fonte do subtítulo
+#' @param cex.axis tamanho da fonte do eixo
+#' @param ... Permite o uso de outros parâmetros gráficos (veja o uso de `density`, nos exemplos)
+#' 
 #' @examples 
 #' plotZ(p = .975)
 #' plotZ(p = .025)
-#' plotZ(z = 1.96)
-#' plotZ(z = -1.96)
+#' plotZ(z = 1.96, density = 20)
+#' plotZ(z = -1.96, density = 40)
+#' plotZ(x = 10, mu = 4.7, dp = 2.7, cor = "yellow")
+#' plotZ(x = 10, mu = 4.7, dp = 2.7, sentido = "maior")
 #' plotZ(p =  .975, sentido = "maior")
 #' plotZ(p =  .025, sentido = "maior")
 #' plotZ(z =  1.96, sentido = "maior")
@@ -22,36 +38,57 @@ plotZ <- function(x = NULL, mu = 0, dp = 1, p = NULL, z = NULL,
   #   cat("ERRO:\tA probabilidade deve estar entre 0 e 1.")
   #   return()
   #   }
+  titulo.menor <- bquote(P(X <= .(x), 
+                         italic(N(bar(x)==.(formatC(mu, decimal.mark = ",")), 
+                         s==.(formatC(dp, decimal.mark = ",")))))
+                         )
+  titulo.maior <- bquote(P(X > .(x), 
+                         italic(N(bar(x)==.(formatC(mu, decimal.mark = ",")), 
+                         s==.(formatC(dp, decimal.mark = ",")))))
+                         )
   
-  # z <- (x - mu)/dp
+  if(!is.null(x)) { 
+    zp <- z <- (x - mu)/dp
+    if(is.null(main)) {
+      if(sentido == "menor") { 
+        main <- titulo.menor
+      } else if(sentido == "maior") {
+        main <- titulo.maior
+      }
+    }
+  }
   
   if(!is.null(p)) {
     zp = qnorm(p) 
   } else if(!is.null(z)) {
     zp = z
-    main = ""
+    # if(is.null(x)) main = ""
   }
   
   if(is.null(main)) {
     main <- expression(italic(N(mu==0, sigma==1)))
   }
+
   if(is.null(sub)) {
     if(sentido == 'menor') {
       if(!is.null(p)) { 
-        prob <- bquote(P(X <= .(round(qnorm(p), 3))) == .(p))
+        prob <- bquote(P(X <= .(formatC(qnorm(p), 3, format = "f", decimal.mark = ","))) == .(p))
         # prob <- bquote(P(X <= .(p)) == .(round(qnorm(p), 3)))
       } else if(!is.null(z)) {
-        prob <- bquote(P(Z <= .(z)) == .(round(pnorm(z), 3)))
+        prob <- bquote(P(Z <= 
+                         .(formatC(z, 2, format = "f", decimal.mark = ","))) ==
+                         .(formatC(pnorm(z), 3, format = "fg", decimal.mark = ",")))
         }
       } else if (sentido == 'maior') {
           if(!is.null(p)) {
-            prob <- bquote(P(X > .(1-round(qnorm(p), 3))) == .(p))
+            prob <- bquote(P(X > .(formatC(1-qnorm(p), 3, format = "f", decimal.mark = ","))) == .(p))
           } else if(!is.null(z)) {
-            prob <- bquote(P(Z > .(z)) == .(1-round(pnorm(z), 3)))
+            prob <- bquote(P(Z > .(formatC(z, 2, format = "f", decimal.mark = ","))) == .(formatC(1-pnorm(z), 3, format = "f", decimal.mark = ",")))
           }
         }
   } else 
     prob <- sub
+
   d <- rnorm(1000000)
   plot(density(d), lwd=2, xlim = c(-3.5,3.5),
        axes = F, cex.lab = 1.3,
@@ -70,6 +107,3 @@ plotZ <- function(x = NULL, mu = 0, dp = 1, p = NULL, z = NULL,
   }
   polygon(x.val, y.val, col = cor, border = NA, ...)
 }
-
-
-#' 
