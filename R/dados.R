8aef382d166b86e3c4930d321773ad546c99f9f2
Fúlvio B. Nedel <fulvionedel@gmail.com>
2023-01-03 00:41:06 -0300

    População do RS 2019

--- R/dados.R
+++ R/dados.R
@@ -22,3 +22,8 @@ NULL
 #' 
 #' @format Banco de dados com estimativas populacionais por sexo e faixa etária para os municípios brasileiros em 2012. Salvo engano que espero ser corrigido, é o último arquivo com estimativas populacionais a esse nível de detalhamento publicado no repositório FTP do DATASUS. O banco é apresentado na forma em que foi baixado de ftp.datasus.gov.br/dissemin/publicos/IBGE/POP/POPBR12.zip. 
 "POPBR12"
+
+#' População por sexo e faixa etária. Municípios gaúchos, 2019.
+#' 
+#' @format Banco de dados com estimativas populacionais por sexo e faixa etária para os municípios do Rio Grande do Sul em 2019. Criado com a função \code{\link[csapAIH]{popbr2000_2021}}, que por sua vez usa os dados tabulados do DATASUS e importados pelo pacote \code{brpop}, de Raphael Saldanha. 
+"POPRS2019"
