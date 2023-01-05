dd66ad99d62733f59b29107a6321193d22cb9ee5
Fúlvio B. Nedel <fulvionedel@gmail.com>
2023-01-05 00:56:49 -0300

    Atualiza a amostra de óbitos com 'process_sim', de 'fetchdatasus'

--- data-raw/obitosBR2019.R
+++ data-raw/obitosBR2019.R
@@ -4,17 +4,18 @@ library(microdatasus)
 library(csapAIH)
 library(dplyr, warn.conflicts = FALSE)
 
-obitosRS2019 <- fetch_datasus(2019, 01, 2019, 12, "RS", "SIM-DO") %>% 
+obitosRS2019 <- fetch_datasus(year_start = 2019, 
+                              year_end = 2019, 
+                              uf = "RS",
+                              information_system = "SIM-DO") %>% 
   slice_sample(prop = .01)
-idadesus <- idadeSUS(obitosRS2019, "SIM")
 
-obitosRS2019 <- obitosRS2019 %>% 
-  rename(sexo = SEXO,
-         idade = IDADE) %>%
-  mutate(sexo = factor(sexo, levels = 1:2, labels = c("masc", "fem")),
-         idade      = idadesus[[1]],
-         fxetar.det = idadesus[[2]],
-         fxetar5    = idadesus[[3]]) 
+obitosRS2019 <- obitosRS2019 %>%
+  bind_cols(idadeSUS(., "SIM")) %>% 
+  select(-IDADE) %>% 
+  process_sim() %>% 
+  mutate(sexo = factor(SEXO, levels = c("Masculino", "Feminino"), labels = c("masc", "fem"))) %>% 
+  select(-SEXO) 
 
 for(i in 1:ncol(obitosRS2019)) {
   if(is.factor(obitosRS2019[,i])) {
