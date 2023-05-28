9f1ab0be657881a1963c0fa2819ff98b63f769c8
Fúlvio B. Nedel <fulvionedel@gmail.com>
2023-05-28 10:13:47 -0300

    Acrescenta 'fxetar3g'

--- R/imp_csapAIH.R
+++ R/imp_csapAIH.R
@@ -1,19 +1,23 @@
-#' @importFrom csapAIH fxetar_quinq ufbr ler_popbr popbr2000_2021 
+#' @importFrom csapAIH fxetar_quinq fxetar3g ufbr ler_popbr popbr2000_2021 
 #' @family csapAIH 
 #' @family DATASUS
 #' 
-#' Transforma um vetor com a idade em faixas etárias quinquenais. A primeira faixa pode ser fracionada em "< 1" e "1-4".
+#' Agrega um vetor com a idade em faixas etárias quinquenais. A primeira faixa pode ser fracionada em "< 1" e "1-4".
 #' @export
 csapAIH::fxetar_quinq
 #' 
-#' Lista as UF e Regiões Administrativas brasileiras.
+#' Agrega um vetor com a idade ou faixas etárias quinquenais em três grandes faixas etárias, "0-14", "15-59" e "60e+" anos de idade.  
+#' @export
+csapAIH::fxetar3g
+#' 
+#' Lista as UF e Regiões Administrativas brasileiras.  
 #' @export
 csapAIH::ufbr
 #' 
-#' Importa arquivos de população do DATASUS até 2012.
+#' Importa arquivos de população do DATASUS até 2012.  
 #' @export
 csapAIH::ler_popbr
 #' 
-#' Estimativas da população 2000-2021 (DATASUS)
+#' Estimativas da população 2000-2021 (DATASUS).  
 #' @export
 csapAIH::popbr2000_2021 
