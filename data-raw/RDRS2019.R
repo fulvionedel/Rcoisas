cdf851dbef2d389d2d7a9f72f08ec189c08e848c
Fúlvio B. Nedel <fulvionedel@gmail.com>
2023-12-13 11:48:30 -0300

    .

--- data-raw/RDRS2019.R
+++ data-raw/RDRS2019.R
@@ -1,9 +1,12 @@
 # Arquivo de dados: Reduzidos da AIH - SIH/SUS. RS, 2019
 
-RDRS2019 <- microdatasus::fetch_datasus(2019, 01, 2019, 12, "RS", "SIH-RD")
+RDRS2019 <- microdatasus::fetch_datasus(2019, 01, 2020, 06, "RS", "SIH-RD") |>
+  dplyr::filter(stringr::str_sub(MUNIC_RES, 1, 2) %in% "43",
+                stringr::str_sub(DT_INTER, 1, 4) %in% "2019") #|>
+  # dplyr::select(N_AIH:SEXO, PROC_REA, DT_INTER:DIAG_PRINC, MUNIC_MOV:IDADE, CNES, MORTE)
 
 for(i in 1:ncol(RDRS2019)) {
-  if(is.factor(RDRS2019[,i])) {
+  if(is.factor(RDRS2019[,i]) | is.character(RDRS2019[,i])) {
     RDRS2019[,i] <- iconv(RDRS2019[,i], to='ASCII//TRANSLIT')
   }
 }
