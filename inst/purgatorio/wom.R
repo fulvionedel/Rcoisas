8a8cbd43ef5e15f0d531eeaa6059cf8eb8ceead0
Fúlvio B. Nedel <fulvionedel@gmail.com>
2024-07-07 20:37:05 -0300

    ajustando gitignore

--- inst/purgatorio/wom.R
+++
@@ -1,6 +0,0 @@
-wom <- function(date) {
-  first <- lubridate::wday(as.Date(
-    paste(lubridate::year(date), lubridate::month(date), 1, sep="-"))
-  )
-  return((lubridate::mday(date)+(first-2)) %/% 7+1)
-}
