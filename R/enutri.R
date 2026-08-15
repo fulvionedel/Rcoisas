1d518113fcfbc7ff3202c3c58e99b20309da8801
Fúlvio B. Nedel <fulvionedel@gmail.com>
2026-08-14 21:21:02 -0300

    Nova função

---
+++ R/enutri.R
@@ -0,0 +1,36 @@
+#' Classificação do estado nutricional de adultos 
+#' @aliases enutri 
+#' 
+#' @description
+#' Classifica o estado nutricional da pessoa segundo valores do Índice de Massa Corporal (IMC), usando os pontos de corte propostos pela Organização Mundial da Saúde. Pode ser informado um vetor com valores do IMC ou um vetor com o peso (Kg) e outro com altura (m^2).
+#' 
+#' @return Um fator com as categorias "Baixo peso", "Eutrófico", "Sobrepeso" ou "Obesidade".
+#' 
+#' @param data Banco de dados com a(s) variável(is) 
+#' @param peso Peso em Kg 
+#' @param altura Altura em m
+#' @param imc Índice de Massa Corporal 
+#' 
+#' @examples
+#' data("hasdm") 
+#' enutri(hasdm) |> 
+#'   tabuleiro()
+#' 
+#' @export 
+#' 
+enutri <- function(data = NULL, peso, altura, imc = NULL) {
+  if(!is.null(data)) {
+    peso <- data$peso
+    altura <- data$altura
+  } else {
+    peso <- {{peso}}
+    altura <- {{altura}}
+  }
+  if(is.null(imc)) {
+    imc <- peso/altura^2
+  }
+  enutri <- cut(imc, breaks = c(-Inf, 18, 25, 30, Inf), right = FALSE,
+                labels = c("Baixo peso", "Eutr\u00f3fico", "Sobrepeso", "Obesidade")
+                )
+  return(enutri)
+}
